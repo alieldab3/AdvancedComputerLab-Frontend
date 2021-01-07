e160483a9ff7c19cf15fd4f1cd2279b1468824d1
Ali Ali El-Dabaa <eldab3.2015@gmail.com>
2021-01-07 07:14:03 +0200

    profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,31 @@
 import React , { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom'
 import './Profile.css';
+import profileImage from "./team2.jpg";
+
+
 
 const Profile =() =>{
     return(
         <div className="main">
-            <h2>Profile</h2>
-            <p>This is Profile</p>
+            <div className="card">
+                <div >
+                <img src={profileImage} alt="John" width='100%' ></img>
+                </div>
+
+                <div>
+                <h1>John Doe</h1>
+                <p class="title">CEO & Founder, Example</p>
+                <p>Harvard University</p>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <p><button>Contact</button></p>
+                </div>
+                
+
+            </div>
         </div>
     )
 }
