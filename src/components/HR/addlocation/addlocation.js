e224999198ba15a5358ba363f5957d884e2b6896
kareem <kareemfathy312@gmail.com>
2021-01-08 20:00:37 +0200

    acas
    ascas

--- src/components/HR/addlocation/addlocation.js
+++ src/components/HR/addlocation/addlocation.js
@@ -1,16 +1,51 @@
 import React , { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom'
+import axios from 'axios'
 import './addlocation.css';
+class addlocation extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    callAPI() {
+        console.log("hi")
+        axios.get("http://localhost:5000/addLocation")            
+            .then(res => console.log(res))
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+            
+    }
 
-const addlocation =() =>{
+    componentDidMount() {
+        this.callAPI();
+    }
+    
+    handleClick= () => {
+     console.log("hi");
+    }
+render() {
     return(
-        <div className="main">
-            <h2>addlocation</h2>
-            <p>This is addlocation</p>
-        </div>
-    )
-}
+        
+<div className="login-page">
+  <div className="form">
+    <form className="login-form">
+      <input type="text"  id="uniqueID"  placeholder="location name"/>
+      <input type="text" placeholder="Capacity"/>
+      <input type="text" placeholder="type"/>
 
+      <button onClick={this.handleClick}  >add location</button>
+      <p className="App-intro">{this.state.apiResponse}</p>
+          
+    </form>
+     
+  </div>
+    
+</div>
+
+)
+    }
+}
 // class viewschedule extends Component {
 //   render () {
 //     return (
