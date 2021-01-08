217709209a01b2648a3b6890a5f0ab2db834b27c
kareem <kareemfathy312@gmail.com>
2021-01-08 21:10:37 +0200

    lol
    lol

--- src/components/HR/addlocation/addlocation.js
+++ src/components/HR/addlocation/addlocation.js
@@ -7,22 +7,45 @@ class addlocation extends Component{
         super(props);
         this.state = { apiResponse: "" };
     }
-    
     callAPI() {
+      let a=document.getElementById("uniqueID").value;
+     let b=document.getElementById("uniqueID1").value;
+     let c=document.getElementById("uniqueID2").value;
+     let d=document.getElementById("uniqueID3").value;
+    console.log(a+" "+b+" "+c+" "+d);
+      let message;
         console.log("hi")
-        axios.get("http://localhost:5000/addLocation")            
-            .then(res => console.log(res))
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-            
+        axios.post('http://localhost:5000/addLocation', {
+          nam:a,
+           cap:b,
+            typ:c,
+           occ:d
+          },{withCredentials:true})
+          .then(function (response) {
+            console.log("fuck you");
+        
+            console.log(response);
+          message.success(response.data);
+          
+        })
+          .catch(function (error){
+            console.log("fuck you1");
+          
+            console.log(error);
+          
+            message.error(error.response.data);
+          });
+          console.log("fuck you2");
+          
     }
 
     componentDidMount() {
-        this.callAPI();
     }
     
     handleClick= () => {
-     console.log("hi");
+     
+     this.callAPI();
+   
     }
 render() {
     return(
@@ -31,8 +54,9 @@ render() {
   <div className="form">
     <form className="login-form">
       <input type="text"  id="uniqueID"  placeholder="location name"/>
-      <input type="text" placeholder="Capacity"/>
-      <input type="text" placeholder="type"/>
+      <input type="text" id="uniqueID1" placeholder="Capacity"/>
+      <input type="text" id="uniqueID2" placeholder="type"/>
+      <input type="text" id="uniqueID3" placeholder="occupation"/>
 
       <button onClick={this.handleClick}  >add location</button>
       <p className="App-intro">{this.state.apiResponse}</p>
