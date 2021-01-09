7aa2dc7979de5e7d99fb960e550203410d4f4457
kareem <kareemfathy312@gmail.com>
2021-01-09 14:22:17 +0200

    nice
    i don't believe the hype

--- src/components/HR/addlocation/addlocation.js
+++ src/components/HR/addlocation/addlocation.js
@@ -3,18 +3,29 @@ import { BrowserRouter , Route } from 'react-router-dom'
 import axios from 'axios'
 import './addlocation.css';
 class addlocation extends Component{
-    constructor(props) {
+  state= {
+    suc:""
+  }  
+  constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+    //    this.state = { apiResponse: "this is my lane boy" };
     }
+
+
+    change = (m) =>{
+      this.setState({
+        suc:m
+      })
+    }
+
     callAPI() {
+       this.change("asdas");  
       let a=document.getElementById("uniqueID").value;
      let b=document.getElementById("uniqueID1").value;
      let c=document.getElementById("uniqueID2").value;
      let d=document.getElementById("uniqueID3").value;
     console.log(a+" "+b+" "+c+" "+d);
       let message;
-        console.log("hi")
         axios.post('http://localhost:5000/addLocation', {
           nam:a,
            cap:b,
@@ -22,20 +33,19 @@ class addlocation extends Component{
            occ:d
           },{withCredentials:true})
           .then(function (response) {
-            console.log("fuck you");
-        
             console.log(response);
           message.success(response.data);
           
         })
+       
           .catch(function (error){
-            console.log("fuck you1");
-          
+        
+            console.log("no")
+      
             console.log(error);
           
             message.error(error.response.data);
           });
-          console.log("fuck you2");
           
     }
 
@@ -57,10 +67,10 @@ render() {
       <input type="text" id="uniqueID1" placeholder="Capacity"/>
       <input type="text" id="uniqueID2" placeholder="type"/>
       <input type="text" id="uniqueID3" placeholder="occupation"/>
-
+       
       <button onClick={this.handleClick}  >add location</button>
-      <p className="App-intro">{this.state.apiResponse}</p>
-          
+      <p className="App-intro">{this.state.suc}</p>
+         
     </form>
      
   </div>
