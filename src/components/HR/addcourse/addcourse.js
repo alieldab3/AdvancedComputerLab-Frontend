f00a23cd215568862b3d1c952487efb572531b79
kareem <kareemfathy312@gmail.com>
2021-01-10 02:18:43 +0200

    maybe
    maybe

--- src/components/HR/addcourse/addcourse.js
+++ src/components/HR/addcourse/addcourse.js
@@ -1,16 +1,92 @@
 import React , { Component } from 'react';
-import { BrowserRouter , Route } from 'react-router-dom'
+import axios from 'axios'
 import './addcourse.css';
+import 'antd/dist/antd.css';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { message, Button ,space} from 'antd';
 
-const addcourse =() =>{
+
+
+const success = () => {
+    
+  message.success('Course created successfully and added to specific department');
+};
+
+const error1 = () => {
+  message.error('Cannot create Course already exists or Department doesnot exist');
+};
+  
+class addcourse extends Component{
+  
+   
+    
+
+
+    callAPI(e) {
+      let a=document.getElementById("uniqueID").value;
+      let b=document.getElementById("uniqueID1").value;
+      let c=document.getElementById("uniqueID2").value;
+   
+      if(a&&b&&c){
+      {e.preventDefault()};
+      console.log(a+" "+b);
+    axios.post('http://localhost:5000/addCourse', {
+        depname:a,
+        nam:b,
+        id:c  
+          },{withCredentials:true})
+          .then(function (response) {
+           {success()}
+           console.log(response);
+          
+        })
+       
+          .catch(function (error){
+        {error1()}
+            console.log("no")
+      
+            console.log(error);
+          
+      //      message.error(error.response.data);
+          });
+   }  
+    }
+
+    componentDidMount() {
+    
+    }
+    
+    handleClick= (e) => {
+
+     this.callAPI(e);
+   
+    }
+render() {
     return(
-        <div className="main">
-            <h2>addcourse</h2>
-            <p>This is addcourse</p>
-        </div>
+<div class="container">  
+  <form id="contact" action="" method="post">
+  <h3>Add course</h3>
+    <h4>Add the course u want using already existed Department!</h4>
+    <form method="form">
+    	<input type="text" id="uniqueID" className="hi" name="u" placeholder="Department Name" required="required" />
+        <input type="text" id="uniqueID1" className="hi" name="u" placeholder="Course Name" required="required" />
+        <input type="text" id="uniqueID2" className="hi" name="u" placeholder="Course id or code" required="required" />
+        
+       
+    <button type="submit"   className="btn btn-primary btn-block btn-large"onClick={this.handleClick} >Create Department Under faculty</button>
+    </form>
+     
+  </form>
+ 
+  
+</div>
+
     )
 }
 
+
+}
 // class viewschedule extends Component {
 //   render () {
 //     return (
--- src/components/HR/addlocation/addlocation.js
+++ src/components/HR/addlocation/addlocation.js
@@ -1,76 +1,85 @@
 import React , { Component } from 'react';
-import { BrowserRouter , Route } from 'react-router-dom'
 import axios from 'axios'
 import './addlocation.css';
+import 'antd/dist/antd.css';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { message, Button ,space} from 'antd';
+
+
+const info = () => {
+  message.info('This is a normal message');
+};
+const success = () => {
+  message.success('Location added successfully');
+};
+
+const error1 = () => {
+  message.error('Sorry cannot add this location either some info is missed or it already exists ');
+};
 class addlocation extends Component{
-  state= {
-    suc:""
-  }  
-  constructor(props) {
-        super(props);
-    //    this.state = { apiResponse: "this is my lane boy" };
-    }
+  
+   
+    
 
 
-    change = (m) =>{
-      this.setState({
-        suc:m
-      })
-    }
-
-    callAPI() {
-       this.change("asdas");  
+    callAPI(e) {
       let a=document.getElementById("uniqueID").value;
      let b=document.getElementById("uniqueID1").value;
      let c=document.getElementById("uniqueID2").value;
      let d=document.getElementById("uniqueID3").value;
     console.log(a+" "+b+" "+c+" "+d);
-      let message;
-        axios.post('http://localhost:5000/addLocation', {
+    if(a&&b&&c&&d){ 
+      {e.preventDefault()};
+    
+     
+      axios.post('http://localhost:5000/addLocation', {
           nam:a,
            cap:b,
             typ:c,
            occ:d
           },{withCredentials:true})
           .then(function (response) {
-            console.log(response);
-          message.success(response.data);
+            {success()}
           
         })
        
           .catch(function (error){
-        
+        {error1()}
             console.log("no")
       
             console.log(error);
           
-            message.error(error.response.data);
+      //      message.error(error.response.data);
           });
-          
+        }
     }
 
-    componentDidMount() {
+    componentDidMount(e) {
     }
     
-    handleClick= () => {
-     
-     this.callAPI();
+    handleClick= (e) => {
+      
+     this.callAPI(e);
    
     }
 render() {
     return(
         
 <div className="login-page">
+<h2 className="formal" >  Add location</h2>
+    
   <div className="form">
     <form className="login-form">
-      <input type="text"  id="uniqueID"  placeholder="location name"/>
-      <input type="text" id="uniqueID1" placeholder="Capacity"/>
-      <input type="text" id="uniqueID2" placeholder="type"/>
-      <input type="text" id="uniqueID3" placeholder="occupation"/>
-       
+
+      <input type="text"  id="uniqueID"  placeholder="location name" required="required"/>
+      <input type="text" id="uniqueID1" placeholder="Capacity" required="required"/>
+      <input type="text" id="uniqueID2" placeholder="type"required="required"/>
+      <input type="text" id="uniqueID3" placeholder="occupation"required="required"/>
+
+
       <button onClick={this.handleClick}  >add location</button>
-      <p className="App-intro">{this.state.suc}</p>
-         
+       
     </form>
      
   </div>
