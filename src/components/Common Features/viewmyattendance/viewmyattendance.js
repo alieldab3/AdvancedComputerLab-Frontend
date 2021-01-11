7b264beaec247d0962923cf728b538baaf6966cd
Ali Ali El-Dabaa <eldab3.2015@gmail.com>
2021-01-12 00:21:24 +0200

    resetpass

--- src/components/Common Features/viewmyattendance/viewmyattendance.js
+++ src/components/Common Features/viewmyattendance/viewmyattendance.js
@@ -1,28 +1,76 @@
 import React , { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom'
+import { Link, NavLink} from 'react-router-dom';
+
+import axios from 'axios';
+
 import './viewmyattendance.css';
 
-const viewmyattendance =() =>{
+class viewmyattendance extends Component{
+
+    state ={
+        attendanceRecords : [],
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/ViewAttendance/:')
+          .then(res => {
+            const attendanceRecords = res.data;
+            this.setState({ attendanceRecords });
+          })
+      }
+
+    render(){
+        let attendanceRecords = this.state.attendanceRecords
+         let RecordDiv = [];
+
+        for (let i = 0; i < attendanceRecords.length; i++) {
+            RecordDiv[i] = (<div className="mb-3">
+            <label for={i+1} class="form-label">Attendance Record {i+1}</label>
+            <input type="text" class="form-control" id={i+1} placeholder="My Name" value={'._id: '+attendanceRecords[i]._id + '  signIn: ' + attendanceRecords[i].signIn} disabled></input>
+            </div> )
+        }
+
     return(
-        <div className="main">
-            <h2>viewmyattendance</h2>
-            <p>This is viewmyattendance</p>
+        <div className="mainP">
+        <div className="container">
+        <div className = 'cardupdate'>
+        <form className="form-floating">
+
+
+        <div>
+        <select class="form-select" aria-label="Default select example">
+        <option selected>Select Month</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        </select>
         </div>
+
+        <div className='Under' >
+
+        <NavLink activeClassName='UpdateProfile' to ='updateprofile'>
+        <button type="button" class="btn btn-primary">Update Profile</button>
+        </NavLink>
+        </div>
+
+        {RecordDiv}
+
+        {/* <div className="mb-3">
+        <label for="FormControlName" class="form-label">Name</label>
+        <input type="text" class="form-control" id="FormControlName" placeholder="My Name" value={this.state.attendanceRecords} disabled></input>
+        </div> */}
+
+        </form>
+        </div>
+        </div>
+        </div>
+
+
     )
+    }
 }
 
-// class viewschedule extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//       <Sidebar />
-//       {/* <Route exact path='/' component={Index} />
-//       <Route path='/contact' component={Contact} /> */}
-//     </BrowserRouter>
-
-//    );
-//   }
-// }
 
 
 export default viewmyattendance;
