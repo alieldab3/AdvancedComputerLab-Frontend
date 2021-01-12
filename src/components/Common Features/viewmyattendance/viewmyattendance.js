57759f9cfb4347b20cbd4c06bffc0bd4ef09d0af
Ali Ali El-Dabaa <eldab3.2015@gmail.com>
2021-01-12 02:02:10 +0200

    viewallattend

--- src/components/Common Features/viewmyattendance/viewmyattendance.js
+++ src/components/Common Features/viewmyattendance/viewmyattendance.js
@@ -10,6 +10,7 @@ class viewmyattendance extends Component{
 
     state ={
         attendanceRecords : [],
+        viewMonth : null,
     }
 
     componentDidMount() {
@@ -20,16 +21,41 @@ class viewmyattendance extends Component{
           })
       }
 
-    render(){
-        let attendanceRecords = this.state.attendanceRecords
-         let RecordDiv = [];
+      ViewRecords(){
+        let recordSelectValue = document.getElementById("FormControlRecords").value;
 
-        for (let i = 0; i < attendanceRecords.length; i++) {
-            RecordDiv[i] = (<div className="mb-3">
-            <label for={i+1} class="form-label">Attendance Record {i+1}</label>
-            <input type="text" class="form-control" id={i+1} placeholder="My Name" value={'._id: '+attendanceRecords[i]._id + '  signIn: ' + attendanceRecords[i].signIn} disabled></input>
-            </div> )
+        if(recordSelectValue=="All"){
+            this.setState({viewMonth : false})
         }
+        else if(recordSelectValue!=null){
+            this.setState({viewMonth : true})
+
+            axios.get('http://localhost:5000/ViewAttendance/'+recordSelectValue)
+            .then(res => {
+              const attendanceRecords = res.data;
+              this.setState({ attendanceRecords });
+            })
+        }
+      }
+
+    render(){
+        let isViewMonth = this.state.viewMonth
+        let attendanceRecords = this.state.attendanceRecords
+        let RecordDiv = [];
+
+        if (isViewMonth == false) {
+            for (let i = 0; i < attendanceRecords.length; i++) {
+                RecordDiv[i] = (<div className="mb-3">
+                <label for={i+1} class="form-label">Attendance Record {i+1}</label>
+                <input type="text" class="form-control" id={i+1} placeholder="My Name" value={'._id: '+attendanceRecords[i]._id + '  signIn: ' + attendanceRecords[i].signIn} disabled></input>
+                </div> )
+            }
+        }
+
+        else if (isViewMonth == true) {
+            /////////////////////////////////////////////////////////////////////////////////
+        }
+
 
     return(
         <div className="mainP">
@@ -39,17 +65,26 @@ class viewmyattendance extends Component{
 
         <div class="row">
             <div class="col">
-                <select class="form-select" aria-label="Default select example">
-                <option selected>Select Month</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
+                <select class="form-select" id="FormControlRecords" aria-label="Select Record">
+                <option selected value="All">All My Records</option>
+                <option value="January">January</option>
+                <option value="February">February</option>
+                <option value="March">March</option>
+                <option value="April">April</option>
+                <option value="May">May</option>
+                <option value="June">June</option>
+                <option value="July">July</option>
+                <option value="August">August</option>
+                <option value="September">September</option>
+                <option value="October">October</option>
+                <option value="November">November</option>
+                <option value="December">December</option>
                 </select>
             </div>
 
             <div class="col">
                 <div className='Under' >
-                <button type="button" class="btn btn-primary">View Month Records</button>
+                <button type="button" class="btn btn-primary"onClick={() => this.ViewRecords()}>View Records</button>
                 </div>
             </div>
         </div>
@@ -57,15 +92,10 @@ class viewmyattendance extends Component{
 
         <br></br>
 
-        <div>
-        {RecordDiv}
-
+        <div >
+            <p>{attendanceRecords.length}</p>
+            {RecordDiv}
         </div>
-
-        {/* <div className="mb-3">
-        <label for="FormControlName" class="form-label">Name</label>
-        <input type="text" class="form-control" id="FormControlName" placeholder="My Name" value={this.state.attendanceRecords} disabled></input>
-        </div> */}
 
         </form>
         </div>
