094e1e7beb20c6c1a055fcdb8599ceb133cd7367
kareem <kareemfathy312@gmail.com>
2021-01-14 22:48:06 +0200

    nice try
     nice try

--- src/components/HOD/assigninstructor/assigninstructor.js
+++ src/components/HOD/assigninstructor/assigninstructor.js
@@ -1,28 +1,133 @@
 import React , { Component } from 'react';
-import { BrowserRouter , Route } from 'react-router-dom'
+import axios from 'axios'
 import './assigninstructor.css';
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
+import { message} from 'antd';
 
-const assigninstructor =() =>{
+
+const success = () => {
+    
+  message.success('Faculty created successfully');
+};
+
+const error1 = () => {
+  message.error('Cannot create faculty already exists' );
+};
+
+  
+class assigninstructor extends Component{
+  
+   
+   
+  state ={
+    token : this.props.token
+  }
+  
+   
+    
+
+
+    callAPI() {
+      let a=document.getElementById("uniqueID").value;
+      let b=document.getElementById("uniqueID1").value;
+   
+      if(!a){
+
+      }
+   else{
+
+    axios.put('http://localhost:5000/addInstructor/ac-30/:CSEN-704', {
+         
+    id:a,
+          course:b
+           
+          },{
+            headers: {'auth-token': this.state.token}
+            }
+            ,{withCredentials:true})
+          .then(function (response) {
+           success()
+           console.log(response);
+          
+        })
+       
+          .catch(function (error){
+        error1()
+            console.log("no")
+      
+            console.log(error);
+          
+      //      message.error(error.response.data);
+          });
+   }  
+    }
+
+    componentDidMount() {
+    
+    }
+    
+    handleClick= () => {
+
+     this.callAPI();
+   
+    }
+render() {
     return(
-        <div className="main">
-            <h2>assigninstructor</h2>
-            <p>This is assigninstructor</p>
+
+        <div className="mainP">
+        <div className="container">
+
+
+        <div className = 'cardupdate'>
+
+        <form class="form-floating">
+            {/* class="form-control is-invalid" */}
+
+        <p> assign course instructor for each course in his department</p>
+
+        
+        <div class="mb-3">
         </div>
-    )
+        <br></br>
+        
+        <ul>
+        </ul>
+
+         <div >
+         <label for="NewPassword" class="form-label" >ID </label>
+            <div class="input-group mb-3">
+            <input typeclass="form-control" id="uniqueID" placeholder="id"></input>
+            </div>
+         </div>
+
+
+         <div>
+         <label for="ConfirmPassword" class="form-label" >Course Name</label>
+        <div class="input-group mb-3">
+        <input type class="form-control" id="uniqueID1" placeholder="course"></input>
+        </div>
+        </div>
+
+
+
+
+        <div className="right">
+        <button type="button" class="btn btn-primary" onClick= {this.handleClick}>Assign instructor</button>
+        </div>
+
+        
+        </form> 
+        
+        </div>
+        </div>
+        </div>
+
+)
+
 }
 
-// class viewschedule extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//       <Sidebar />
-//       {/* <Route exact path='/' component={Index} />
-//       <Route path='/contact' component={Contact} /> */}
-//     </BrowserRouter>
 
-//    );
-//   }
-// }
-
+}
 
 export default assigninstructor;
