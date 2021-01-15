ab904d71ce3eca4afa0b30d36428e66b49712d77
kareem <kareemfathy312@gmail.com>
2021-01-15 15:48:44 +0200

    HAHAHAHHAHAHAHAHAHAH
    HAHAHAHHAHAHAHAHAHAH

--- src/components/Course Instructor/updateassignment/updateassignment.js
+++ src/components/Course Instructor/updateassignment/updateassignment.js
@@ -1,28 +1,169 @@
 import React , { Component } from 'react';
-import { BrowserRouter , Route } from 'react-router-dom'
+import axios from 'axios'
 import './updateassignment.css';
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
+import { message} from 'antd';
+import { Table, Tag, Space } from 'antd';
+const columns = [
+  {
+    title: 'id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'dayoff',
+    dataIndex: 'dayoff',
+    key: 'dayoff',
+  },
+]
+let data=[];
+   
+const success = () => {
+    
+  message.success('Slot assigning Updated  successfully');
+};
 
-const updateassignment =() =>{
+const error1 = () => {
+  message.error('Cannot create faculty already exists' );
+};
+const warning = () => {
+    
+  message.warning('Plz fill all the required information');
+};
+
+  
+class updateassignment extends Component{
+  
+   
+   
+  state ={
+    token : this.props.token,
+    arr:[]
+  }
+  
+   
+    
+
+
+    callAPI() {
+      let a=document.getElementById("uniqueID").value;
+      let b=document.getElementById("uniqueID1").value;
+      let c=document.getElementById("uniqueID2").value;
+   
+      if(a&&b&&c){
+   
+success();
+      }
+   else{
+       
+   warning();
+/*    axios.put('http://localhost:5000/addInstructor/ac-30/:CSEN-704', {
+         
+    id:a,
+          course:b
+           
+          },{
+            headers: {'auth-token': this.state.token}
+            }
+            ,{withCredentials:true})
+          .then(function (response) {
+           success()
+           console.log(response);
+          
+        })
+       
+          .catch(function (error){
+        error1()
+            console.log("no")
+      
+            console.log(error);
+          
+      //      message.error(error.response.data);
+          });*/
+   }  
+    }
+
+    componentDidMount() {
+    
+    }
+    
+    handleClick= () => {
+
+     this.callAPI();
+   
+    }
+render() {
     return(
-        <div className="main">
-            <h2>updateassignment</h2>
-            <p>This is updateassignment</p>
+
+        <div className="mainP">
+
+        <div className="container">
+
+
+        <div className = 'cardupdate'>
+
+        <form class="form-floating">
+            {/* class="form-control is-invalid" */}
+
+        <p>Update Assigned Slots</p>
+
+        
+        <div class="mb-3">
         </div>
-    )
+        <br></br>
+        
+        <ul>
+        </ul>
+
+         <div >
+         <label for="NewPassword" class="form-label" >ID </label>
+            <div class="input-group mb-3">
+            <input typeclass="form-control" id="uniqueID" placeholder="id"></input>
+            </div>
+            <div>
+            <br></br>
+        
+            <input typeclass="form-control" id="uniqueID1" placeholder="Course Name"></input>
+            </div>
+           <div>
+           <br></br>
+        
+            <input typeclass="form-control" id="uniqueID2" placeholder="Slot id"></input>
+            </div>
+            
+
+         </div>
+
+         <div>
+        <div class="input-group mb-3">
+        </div>
+        </div>
+
+
+
+
+        <div className="right">
+        <button type="button" class="btn btn-primary" onClick= {this.handleClick}>Update</button>
+        </div>
+
+        
+        </form> 
+        
+        </div>
+        </div>
+        </div>
+
+)
+
 }
 
-// class viewschedule extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//       <Sidebar />
-//       {/* <Route exact path='/' component={Index} />
-//       <Route path='/contact' component={Contact} /> */}
-//     </BrowserRouter>
 
-//    );
-//   }
-// }
-
+}
 
 export default updateassignment;
