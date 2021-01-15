597e9c73222e2420942f39752e8098930201864b
mohamedFabdrabo <42771081+mohamedFabdrabo@users.noreply.github.com>
2021-01-15 22:00:52 +0200

    fathy-CourseCoordinator1.1

--- src/components/Course Coordinator/controlrequests/controlrequests.js
+++ src/components/Course Coordinator/controlrequests/controlrequests.js
@@ -1,28 +1,115 @@
 import React , { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom'
 import './controlrequests.css';
+import axios from 'axios';
+ class controlrequests extends Component {
+    state ={
+        token : this.props.token,
+        AllRequests : []
+    }
+    componentDidMount() {
+        axios.get('http://localhost:5000/coordinator/viewSlotLinking',{
+            headers: {
+                'auth-token': this.state.token
+            }
+          })
+          .then(res => {
+            const AllRequests = res.data;
+            this.setState({ AllRequests  :  AllRequests});
+        })
+      }
+      AcceptRequest(rid)
+        {
+        axios.put('http://localhost:5000/coordinator/acceptSlotLinking',{   
+            request_id:rid
+            },{
+                headers: {
+                    'auth-token': this.state.token
+                },
+                
+              },{withCredentials:true})
+            .then((response)=>{
+                this.setState({message : "Accepted Sucessfully"})
+                this.componentDidMountAgain();
+            })
+            .catch((error)=>{
+                this.setState({message : "Acceptance failed"})
+                console.log(error);
+            });    
+        }
+        RejectRequest(rid)
+        {
+        axios.put('http://localhost:5000/coordinator/rejectSlotLinking',{   
+            request_id:rid
+            },{
+                headers: {
+                    'auth-token': this.state.token
+                },
+                
+              },{withCredentials:true})
+            .then((response)=>{
+                this.setState({message : "Rejected Sucessfully"})
+                this.componentDidMountAgain();
+            })
+            .catch((error)=>{
+                this.setState({message : "Rejection failed"})
+                console.log(error);
+            });    
+        }
 
-const controlrequests =() =>{
-    return(
-        <div className="main">
-            <h2>controlrequests</h2>
-            <p>This is controlrequests</p>
+
+   render () {
+    let RenderSlotLinking = [];
+    for(let elem of this.state.AllRequests)
+    {
+        if(this.state.R_state.localeCompare("0")==0 || this.state.R_state.localeCompare(elem.state)==0)
+        {
+        let cancelEn = (elem.state.localeCompare("Pending")==0 || elem.state.localeCompare("Accepted")==0)?true:false;
+       
+            RenderSlotLinking.push(
+                <tr class = "fathy_row">
+                <th scope="row">{elem.rid}</th>
+                <td>{elem.type}</td>
+                <td>{elem.slot}</td>
+                <td>{elem.state}</td>
+                <td>{elem.sender}</td>
+                <td>{elem.senderComment}</td>
+                <td><button type="button"  id = {"Accept"+elem.id} onClick={(e)=>this.AcceptRequest(elem.rid)} class="btn btn-success"disabled = {!cancelEn}>Accept</button></td>
+                <td><button type="button"  id = {"Reject"+elem.id} onClick={(e)=>this.RejectRequest(elem.rid)} class="btn btn-success"disabled = {!cancelEn}>Reject</button></td>
+                </tr>
+            );
+        }
+    }
+    let Slot_Linking_Table = (
+        <div>
+        <table id = "Slot_Linking_Table" class="table table-striped">
+        <thead>
+            <tr class = "fathy_row">
+            <th scope="col">Id</th>
+            <th scope="col">Request Type   </th>
+            <th scope="col">Requested slot </th>
+            <th scope="col">Request State  </th>
+            <th scope="col">Sender          </th>
+            <th scope="col">Sender Comment </th>
+            <th scope="col">Accept</th>
+            <th scope="col">Reject</th>
+            </tr>
+        </thead>
+        <tbody>
+            {RenderSlotLinking}
+        </tbody>
+        </table>
+        <br></br><br></br><br></br>
         </div>
-    )
-}
-
-// class viewschedule extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//       <Sidebar />
-//       {/* <Route exact path='/' component={Index} />
-//       <Route path='/contact' component={Contact} /> */}
-//     </BrowserRouter>
-
-//    );
-//   }
-// }
+    );
+     return (
+         <div class = "main">
+             <label for="Slot_Linking_Table" class="form-label"><h1>Slot-Linking Requests</h1></label>
+             {Slot_Linking_Table}
+         </div>
+    );
+   }
+ }
 
 
 export default controlrequests;
--- src/components/Course Coordinator/deletecourseslot/deletecourseslot.js
+++ src/components/Course Coordinator/deletecourseslot/deletecourseslot.js
@@ -1,28 +1,89 @@
 import React , { Component } from 'react';
 import { BrowserRouter , Route } from 'react-router-dom'
 import './deletecourseslot.css';
+import axios from 'axios';
+class deletecourseslot extends Component{
+    state = {
+        token:this.props.token,
+        message:"",
+        AllSlots:[]
+    }
+    componentDidMountAgain(){
+        axios.get('http://localhost:5000/coordinator/AllCourseSlots',{
+            headers: {
+                'auth-token': this.state.token
+            }
+          })
+          .then(res => {
+              console.log("Get All Slots");
+              let AllSlots = res.data;
+              this.setState({AllSlots:AllSlots});
+          });
+    }
+    componentDidMount() {
+        axios.get('http://localhost:5000/coordinator/AllCourseSlots',{
+            headers: {
+                'auth-token': this.state.token
+            }
+          })
+          .then(res => {
+              console.log("Get All Slots");
+              let AllSlots = res.data;
+              this.setState({AllSlots:AllSlots});
+          });
+      }
+    HandleDelete()
+    {
+        let sid = document.getElementById("SlotToDelete").value;
+        if(sid.localeCompare("Select") == 0)
+        {
+            this.setState({message:"Please Select slot"});
+            return;
+        }
+        axios.delete('http://localhost:5000/coordinator/deleteSlot',{
+                headers: {
+                    'auth-token': this.state.token
+                },
+                data:{   
+                    sid:sid
+                }
+              },{withCredentials:true})
+            .then((response)=>{
+                this.setState({message : "Slot deleted Sucessfully"})
+                this.componentDidMountAgain();
+            })
+            .catch((error)=>{
+                this.setState({message : "deletion failed"})
+                console.log(error);
+            });    
+    }
+    render(){
+        let options = [];
+        for(let elem of this.state.AllSlots)
+        {
+            options.push(<option value={elem.sid}>{"Day: "+elem.day + " Timing: "+elem.timing + " Location: "+elem.location}</option>
+            );
+        }
 
-const deletecourseslot =() =>{
-    return(
-        <div className="main">
-            <h2>deletecourseslot</h2>
-            <p>This is deletecourseslot</p>
-        </div>
-    )
+        return(
+            <div class="main">
+            <div class="cardupdate">
+                <h3>Select The Slot You Want to delete</h3>
+                <select class="form-select" id="SlotToDelete" aria-label="Default select example">
+                <option selected>Select</option>
+                {options}
+                </select><br></br><br></br>
+                <button type="button" class="btn btn-danger" onClick={() => this.HandleDelete()}>Delete Slot</button>
+                <br></br><br></br>
+                <h3>{this.state.message}</h3>
+
+            </div>   
+            </div>
+        )}
 }
 
-// class viewschedule extends Component {
-//   render () {
-//     return (
-//       <BrowserRouter>
-//       <Sidebar />
-//       {/* <Route exact path='/' component={Index} />
-//       <Route path='/contact' component={Contact} /> */}
-//     </BrowserRouter>
 
-//    );
-//   }
-// }
+
 
 
 export default deletecourseslot;
